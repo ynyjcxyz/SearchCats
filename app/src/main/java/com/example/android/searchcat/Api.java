7b0f8c66b6500835c5e82358d86c04a5a61ab2f4
John Young <ynyjcxyz@gmail.com>
2021-10-27 03:03:48 -0700

    first commit, failed to get the tag list

---
+++ app/src/main/java/com/example/android/searchcat/Api.java
@@ -0,0 +1,12 @@
+package com.example.android.searchcat;
+
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface Api {
+    String BASE_URL = "https://cataas.com/";
+    @GET("api/{tags}")
+    Call<List<AnimalNames>> getTags(@Path("tags")String tags);
+}
---
+++ app/src/main/java/com/example/android/searchcat/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.android.searchcat;
+
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+    // Declare Variables
+    ListView list;
+    ListViewAdapter adapter;
+    SearchView editSearch;
+    ArrayList<AnimalNames> arraylist = new ArrayList<AnimalNames>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getTags();
+
+//      Generate sample data
+//        String[] tagsArray = new String[]{"Lion", "Tiger", "Dog",
+//                "Cat", "Tortoise", "Rat", "Elephant", "Fox",
+//                "Cow", "Donkey", "Monkey"};
+
+        // Locate the ListView in listview_main.xml
+//        list = (ListView) findViewById(R.id.listview);
+//        for (Object o : tagsArray) {
+//            AnimalNames animalNames = new AnimalNames((String) o);
+//            // Binds all strings into an array
+//            arraylist.add(animalNames);
+//        }
+
+
+        // Pass results to ListViewAdapter Class
+//        adapter = new ListViewAdapter(this, arraylist);
+
+        // Binds the Adapter to the ListView
+//        list.setAdapter(adapter);
+
+        // Locate the EditText in listview_main.xml
+//        editSearch = (SearchView) findViewById(R.id.search_view);
+//        editSearch.setOnQueryTextListener(this);
+
+    }
+
+    private void getTags() {
+        Call<List<AnimalNames>> call = RetrofitClient.getInstance().getMyApi().getTags("tags");
+        call.enqueue(new Callback<List<AnimalNames>>() {
+            @Override
+            public void onResponse(@NonNull Call<List<AnimalNames>> call, Response<List<AnimalNames>> response) {
+                List<AnimalNames> tagsList = response.body();
+                assert tagsList != null;
+                arraylist.addAll(tagsList);
+                list = (ListView) findViewById(R.id.listview);
+
+                adapter = new ListViewAdapter(MainActivity.this, arraylist);
+                list.setAdapter(adapter);
+                editSearch = (SearchView) findViewById(R.id.search_view);
+                editSearch.setOnQueryTextListener(MainActivity.this);
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<List<AnimalNames>> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), "An error has occurred", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        String text = newText;
+        adapter.filter(text);
+        return false;
+    }
+}
